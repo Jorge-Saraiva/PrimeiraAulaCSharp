1cfa0e4bd74fe5de76aa6484b8089ce2c9e507ad
fenixgaspar <fenixgaspar@gmail.com>
2023-08-09 22:44:46 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiraAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mais uma linha!");
+
+            string nome = "Maria José";
+
+            Console.WriteLine("O nome da pessoa é " + nome);
+
+            Console.Write("Entre com um número: ");
+            int numero = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("O número digitado foi: " + numero);
+            
+            Console.ReadKey();
+        }
+    }
+}
